a8b9f510e346be41172fffca4cf007f936c63b93
git4impatient <lurie@lurie.biz>
2017-08-30 12:37:34 -0400

    Create creds.sh

---
+++ creds.sh
@@ -0,0 +1,6 @@
+# set up your aws creds here
+# spawn new shell to keep the exports
+
+export AWS_ACCESS_KEY_ID="AxxxxxxxxxxxxxxxxxxA"
+export AWS_SECRET_ACCESS_KEY='9xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxr'
+bash
